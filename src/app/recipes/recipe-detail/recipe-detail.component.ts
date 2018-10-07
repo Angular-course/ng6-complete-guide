b2926ec65493b074cf460b446317ee52e484af61
Adam Morsi <adam.morsi@yahoo.com>
2018-10-07 13:55:35 +0300

    Enf of section 16 (Finished the form of recipes)

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -5,32 +5,38 @@ import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+    selector: 'app-recipe-detail',
+    templateUrl: './recipe-detail.component.html',
+    styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  recipe: Recipe;
-  id: number;
+    recipe: Recipe;
+    id: number;
 
-  constructor(private recipeService: RecipeService,
-              private router: Router,
-              private activatedRoute: ActivatedRoute) { }
-
-  ngOnInit() {
-      this.activatedRoute.params.subscribe(
-          (params: Params) => {
-            this.id = +params['id'];
-            this.recipe = this.recipeService.getRecipe(this.id);
-          }
-      );
-  }
-
-    onAddToShoppingList = () => {
-      this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+    constructor(private recipeService: RecipeService,
+                private router: Router,
+                private activatedRoute: ActivatedRoute) {
     }
 
-    onEditRecipe = () => {
-      this.router.navigate(['edit'], {relativeTo: this.activatedRoute});
+    ngOnInit() {
+        this.activatedRoute.params.subscribe(
+            (params: Params) => {
+                this.id = +params['id'];
+                this.recipe = this.recipeService.getRecipe(this.id);
+            }
+        );
+    }
+
+    onAddToShoppingList() {
+        this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+    }
+
+    onEditRecipe() {
+        this.router.navigate(['edit'], {relativeTo: this.activatedRoute});
+    }
+
+    onDeleteRecipe() {
+        this.recipeService.deleteRecipe(this.id);
+        this.router.navigate(['../'], {relativeTo: this.activatedRoute});
     }
 }
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,24 +1,95 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+
+import {RecipeService} from '../recipe.service';
 
 @Component({
-  selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+    selector: 'app-recipe-edit',
+    templateUrl: './recipe-edit.component.html',
+    styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
-  id: number;
-  editMode = false;
+    id: number;
+    editMode = false;
+    recipeForm: FormGroup;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+    constructor(private router: Router,
+                private activatedRoute: ActivatedRoute,
+                private recipeServie: RecipeService) {
+    }
 
-  ngOnInit() {
-    this.activatedRoute.params.subscribe(
-        (params: Params) => {
-            this.id = +params['id'];
-            this.editMode = params['id'] != null;
+    ngOnInit() {
+        this.activatedRoute.params.subscribe(
+            (params: Params) => {
+                this.id = +params['id'];
+                this.editMode = params['id'] != null;
+                this.initForm();
+            }
+        );
+    }
+
+    onSubmit() {
+        if (this.editMode) {
+            this.recipeServie.updateRecipe(this.id, this.recipeForm.value);
+        } else {
+            this.recipeServie.addRecipe(this.recipeForm.value);
         }
-    );
-  }
+        this.router.navigate(['../'], {relativeTo: this.activatedRoute});
+    }
+
+    private initForm() {
+        let recipeName = '';
+        let recipeImagePath = '';
+        let recipeDescription = '';
+        const recipeIngredients = new FormArray([]);
+
+        if (this.editMode) {
+            const recipe = this.recipeServie.getRecipe(this.id);
+            recipeName = recipe.name;
+            recipeImagePath = recipe.imagePath;
+            recipeDescription = recipe.description;
+            if (recipe['ingredients']) {
+                for (const ingredient of recipe.ingredients) {
+                    recipeIngredients.push(
+                        new FormGroup({
+                            'name': new FormControl(ingredient.name,
+                                Validators.required),
+                            'amount': new FormControl(ingredient.amount,
+                                [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
+                        })
+                    );
+                }
+            }
+        }
+        this.recipeForm = new FormGroup({
+            'name': new FormControl(recipeName, Validators.required),
+            'imagePath': new FormControl(recipeImagePath, Validators.required),
+            'description': new FormControl(recipeDescription, Validators.required),
+            'ingredients': recipeIngredients
+        });
+    }
+
+    get recipeIngredientsData() {
+        return <FormArray>this.recipeForm.get('ingredients');
+    }
+
+    onAddIngredient() {
+        (<FormArray>this.recipeForm.get('ingredients')).push(
+            new FormGroup({
+                'name': new FormControl(null, Validators.required),
+                'amount': new FormControl(null,
+                    [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
+            })
+        );
+    }
+
+    onDeleteIngredient(index: number) {
+        (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+    }
+
+    onCancel() {
+        this.router.navigate(['../'], {relativeTo: this.activatedRoute});
+    }
 
 }
