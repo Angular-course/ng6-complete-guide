d58583650e6aef9aa1c1b3e68632db36af1bac30
Adam Morsi <adam.morsi@yahoo.com>
2018-10-11 22:46:28 +0300

    Added the shopping list feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,33 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {RecipeService} from './recipes/recipe.service';
 import {AppRoutingModule} from './app-routing.module';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
 import {DataStorageService} from './shared/data-storage.service';
 import {AuthService} from './auth/auth.service';
 import {AuthGuardService} from './auth/auth-guard.service';
 import {RecipesModule} from './recipes/modules/recipes/recipes.module';
 import {SharedModule} from './shared/modules/shared.module';
 import {AuthModule} from './auth/modules/auth/auth.module';
+import {ShoppingListModule} from './shopping-list/modules/shopping-list/shopping-list.module';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent,
-        ShoppingListComponent,
-        ShoppingEditComponent
+        HeaderComponent
     ],
     imports: [
         BrowserModule,
         AuthModule,
         RecipesModule,
         AppRoutingModule,
-        FormsModule,
         HttpClientModule,
-        SharedModule
+        SharedModule,
+        ShoppingListModule
     ],
     providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService],
     bootstrap: [AppComponent]
