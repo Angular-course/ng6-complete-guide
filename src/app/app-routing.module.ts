5bcc1d11dc26dccb933c3c691b0fd40496ac9247
Adam Morsi <adam.morsi@yahoo.com>
2018-10-12 17:18:32 +0300

    Adding font-awesome package & not-component and add the wild card route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {HomeComponent} from './core/home/home.component';
 import {LazyAuthGuardService} from './auth/lazy-auth-guard.service';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 const appRoutes: Routes = [
     {path: '', component: HomeComponent, pathMatch: 'full'},
     {path: 'recipes', loadChildren: './recipes/modules/recipes/recipes.module#RecipesModule', canLoad: [LazyAuthGuardService]},
-    {path: 'shopping-list', component: ShoppingListComponent}
+    {path: 'shopping-list', component: ShoppingListComponent},
+    {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
@@ -8,10 +9,12 @@ import {SharedModule} from './shared/modules/shared.module';
 import {AuthModule} from './auth/modules/auth/auth.module';
 import {ShoppingListModule} from './shopping-list/modules/shopping-list/shopping-list.module';
 import {CoreModule} from './core/modules/core/core.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        NotFoundComponent
     ],
     imports: [
         BrowserModule,
@@ -20,7 +23,8 @@ import {CoreModule} from './core/modules/core/core.module';
         HttpClientModule,
         SharedModule,
         ShoppingListModule,
-        CoreModule
+        CoreModule,
+        AngularFontAwesomeModule
     ],
     bootstrap: [AppComponent]
 })
